b33e9fbdc93d7e56336d7db9d33e4645684a730f
deedy5 <65482418+deedy5@users.noreply.github.com>
2021-08-11 18:31:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url="https://github.com/deedy5/google_trends",
     license="MIT",
     py_modules=["google_trends"],
-    install_requires=["requests>=2.25.0"],
+    install_requires=["requests>=2.26.0"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
