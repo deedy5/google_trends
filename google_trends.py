187fe3b7ba99da2c512db2345bff49497041e8d8
deedy5 <65482418+deedy5@users.noreply.github.com>
2021-09-03 22:16:07 +0300

    v1.0
    Each id was loaded in a separate json, which could lead to IP blocking, so now:
    1. added cache to eliminate unnecessary repeated requests,
    2. additional requests are formed by 20 ids in a single request,
    3. requests now use the session to use cookies.

--- google_trends.py
+++ google_trends.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-__version__ = '0.9.2'
+__version__ = '1.0'
 
 def gtrends(*args, **kwargs):
     print("Function 'gtrends' is deprecated: renamed to 'daily_trends'")
@@ -13,8 +13,7 @@ def daily_trends(date=None, country='US', language='en-US', timezone='-180'):
     country = 'US', 'RU', etc.;
     language = 'en-US', 'ru-RU', etc.;
     timezone = timezone offset, example: GMT-7 == -7*60 = '-420'.
-    '''
-    
+    '''    
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"}
     params = {
         'hl': language,
@@ -35,8 +34,7 @@ def daily_trends(date=None, country='US', language='en-US', timezone='-180'):
     return res
 
 def realtime_trends(country='US', category='all', language='en-US', num_results=20, timezone='-180'):
-    ''' Google realtime search trends
-    
+    ''' Google realtime search trends    
     country = 'US', 'RU', etc.;
     category = 'all' (all), 'b' (business), 'e' (entertainment), 
                'm' (health), 's' (sports), 't' (sci/tech), 'h' (top stories);
@@ -44,59 +42,76 @@ def realtime_trends(country='US', category='all', language='en-US', num_results=
     num_results = how many results to return, max num_results = 200;
     timezone = timezone offset, example: GMT-7 == -7*60 = '-420'.
     '''
-    
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"}
-    params = {
-        'hl': language,
-        'tz': timezone,
-        'cat': category,
-        'fi': '0',
-        'fs': '0',
-        'geo': country,
-        'ri': '300',
-        'rs': f'{num_results}',
-        'sort': '0',
-        }
-    r = requests.get('https://trends.google.com/trends/api/realtimetrends', headers=headers, params=params)
-    i = r.text.index('{')
-    r = r.text[i:]
-    r = json.loads(r)
-    data = r["storySummaries"]["trendingStories"]
-    res = []
-    for element in data:
-        t = {
-        "title": element["title"],
-        "entity_names": element["entityNames"],
-        "article_urls": [x["url"] for x in element["articles"]],
-        }
-        res.append(t)
-    if len(res) >= num_results:
-        return res
-    else:
-        trending_story_ids = r["trendingStoryIds"]
-        res = []
-        for i in range(min(num_results, len(trending_story_ids))):
-            t = _get_realtime_trend(trending_story_ids[i], language=language, timezone=timezone)
-            if t:
-                res.append(t)
-        return res
-
-def _get_realtime_trend(trending_story_id, language='en-US', timezone='-180'):
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"}
-    params = {
-        'hl': language,
-        'tz': timezone,
-        }
-    r = requests.get(f'https://trends.google.com/trends/api/stories/{trending_story_id}', headers=headers, params=params)
-    i = r.text.index('{')
-    r = r.text[i:]
-    r = json.loads(r)
-    try:
-        t = {
-            "title": r["title"],
-            "entity_names": r["entityNames"],
-            "article_urls": [x["url"] for x in r["widgets"][0]["articles"]],
+    with requests.Session() as s:
+        s.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"})
+        params = {
+            'hl': language,
+            'tz': timezone,
+            'cat': category,
+            'fi': '0',
+            'fs': '0',
+            'geo': country,
+            'ri': '300',
+            'rs': f'{num_results}',
+            'sort': '0',
             }
-        return t
-    except:
-        return
+        # request json with real time trends
+        r = s.get('https://trends.google.com/trends/api/realtimetrends', params=params)
+        # fix the json and load it as a dictionary
+        i = r.text.index('{')
+        r = r.text[i:]
+        r = json.loads(r)
+        # extract information from json
+        data = r["storySummaries"]["trendingStories"]
+        res, ids_cache = [], set()
+        for element in data:
+            res.append({"title": element["title"],
+                        "entity_names": element["entityNames"],
+                        "article_urls": [x["url"] for x in element["articles"]],})
+            ids_cache.add(element["id"])
+
+        # if not enough results, request additional jsons by trending_story_ids
+        trending_story_ids = r["trendingStoryIds"]
+        while len(res) < num_results:
+            id_list = []
+            for trending_story_id in trending_story_ids:
+                if trending_story_id not in ids_cache:
+                    id_list.append(trending_story_id)
+                if len(id_list) == 20:
+                    break
+            if not id_list:
+                break
+            ids_cache.update(id_list)
+            params = {
+                'hl': language,
+                'tz': timezone,
+                'cat': category,
+                'id': id_list,
+                }
+            r = s.get('https://trends.google.com/trends/api/stories/summary', params=params)
+            i = r.text.index('{')
+            r = r.text[i:]
+            r = json.loads(r)
+            data = r["trendingStories"]
+            for element in data:
+                res.append({"title": element["title"],
+                            "entity_names": element["entityNames"],
+                            "article_urls": [x["url"] for x in element["articles"]],})
+                ids_cache.add(element["id"])
+
+        ''' ----- Another way, if not enough results -----
+        # request additional jsons per each trending_story_id
+        trending_story_ids = r["trendingStoryIds"]
+        i = 0
+        while len(res) < num_results and len(res) < len(trending_story_ids):
+            if trending_story_ids[i] not in ids_cache:
+                r = s.get(f'https://trends.google.com/trends/api/stories/{trending_story_ids[i]}?hl={language}&tz={timezone}')
+                i = r.text.index('{')
+                r = r.text[i:]
+                r = json.loads(r)
+                res.append({"title": r["title"],
+                            "entity_names": r["entityNames"],
+                            "article_urls": [x["url"] for x in r["widgets"][0]["articles"]],})
+            i += 1
+        '''
+    return res
